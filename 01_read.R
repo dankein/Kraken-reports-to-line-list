6b9dfe14acbf617e9f6720e3dbebe2a334f55d21
dankein <dankein@users.noreply.github.com>
2019-08-27 11:01:41 -0500

    adding filters

--- 01_read.R
+++ 01_read.R
@@ -5,5 +5,8 @@
 # Use this file for reading in the raw data
 
 # Read Kraken Reports From R Individually
-data<- read.delim("K:/2019-Kraken in R/data/1800786.tabular", header=FALSE)
+data<- read.delim("./data/1800786.tabular", header=FALSE)
 
+files <- list.files(path = /data/ 
+                    pattern = "*.json",
+                    full.names = T)
--- 02_tidy.R
+++ 02_tidy.R
@@ -2,4 +2,22 @@
 # analysis-template/clean/
 # Matt Dray
 
+
 # Use this file for tidying data and preparing it for further analysis
+library(tidyverse)
+
+
+my_data <- as_tibble(data)
+
+# Galaxy workflow filters c5==0 or c5==2 or c5==1763 or c5==77643 or c5==1773 or c5==9606
+
+
+
+#filter Mycobacterium
+unclassified<- my_data %>% filter(V5 == 0)
+
+#filter Bacteria
+bacteria<- my_data %>% filter(V5 == 2)
+
+#filter Mycobacterium
+mycobacterium<- my_data %>% filter(V5 == 1763)
