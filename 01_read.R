d8de282fb63b4bf03f5e895ebc06da9ce950e0e3
dankein <dankein@users.noreply.github.com>
2019-08-27 10:33:11 -0500

    changed

--- 01_read.R
+++ 01_read.R
@@ -3,3 +3,7 @@
 # Matt Dray
 
 # Use this file for reading in the raw data
+
+# Read Kraken Reports From R Individually
+data<- read.delim("K:/2019-Kraken in R/data/1800786.tabular", header=FALSE)
+
