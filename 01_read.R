811a19f43aa93ea5d1eba779a501e47efb62838b
dankein <dankein@users.noreply.github.com>
2019-08-27 15:08:01 -0500

    f

--- 01_read.R
+++
@@ -1,9 +0,0 @@
-# Read data
-# analysis-template/src/
-# Matt Dray
-
-# Use this file for reading in the raw data
-
-# Read Kraken Reports from Galaxy saved in the /data folder (Galaxy outputs .tabular files)
-
-files <- list.files(path="./data", pattern="*.tabular", full.names=TRUE, recursive=FALSE)
---
+++ 01_read_data.R
@@ -0,0 +1,8 @@
+# Read data 
+# from Matt Dray analysis-template/src/
+
+# Use this file for reading in the raw data - Kraken Reports from Galaxy saved in the /data folder (Galaxy outputs .tabular files)
+# Daniel Kein
+# August 2019
+
+files <- list.files(path="./data", pattern="*.tabular", full.names=TRUE, recursive=FALSE)
