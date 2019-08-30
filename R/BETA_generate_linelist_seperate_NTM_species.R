68d06b58260ce5fb8b6ed7c06918f72958262662
dankein <dankein@users.noreply.github.com>
2019-08-30 10:21:15 -0500

    update

--- R/BETA_generate_linelist_seperate_NTM_species.R
+++ R/BETA_generate_linelist_seperate_NTM_species.R
@@ -141,9 +141,201 @@ make_kraken <- function(files) {
   } else{
     Mycobacterium_avium <- my_data %>% filter(V5 == 1764)
     Mycobacterium_avium <-
-      Mycobacterium_avium[, c("V1")]
-  }
+      Mycobacterium_avium[, c("V1")]}
   
+  
+  
+  #Mycobacterium_avium_subsp_avium 44454
+  Mycobacterium_avium_subsp_avium <- my_data %>% filter(V5 == 44454)
+  Mycobacterium_avium_subsp_avium <-
+    Mycobacterium_avium_subsp_avium[, c("V1")]
+  if (nrow(Mycobacterium_avium_subsp_avium) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_avium_subsp_avium <- data.frame(V1)
+  } else{
+    Mycobacterium_avium_subsp_avium <- my_data %>% filter(V5 == 44454)
+    Mycobacterium_avium_subsp_avium <-
+      Mycobacterium_avium_subsp_avium[, c("V1")]}
+  
+  #Mycobacterium_avium_subsp_hominissuis  439334
+  Mycobacterium_avium_subsp_hominissuis <- my_data %>% filter(V5 == 439334)
+  Mycobacterium_avium_subsp_hominissuis <-
+    Mycobacterium_avium_subsp_hominissuis[, c("V1")]
+  if (nrow(Mycobacterium_avium_subsp_hominissuis) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_avium_subsp_hominissuis <- data.frame(V1)
+  } else{
+    Mycobacterium_avium_subsp_hominissuis <- my_data %>% filter(V5 == 439334)
+    Mycobacterium_avium_subsp_hominissuis <-
+      Mycobacterium_avium_subsp_hominissuis[, c("V1")]}
+  
+  
+  #Mycobacterium_avium_subsp_paratuberculosis  1770
+  Mycobacterium_avium_subsp_paratuberculosis <- my_data %>% filter(V5 == 1770)
+  Mycobacterium_avium_subsp_paratuberculosis <-
+    Mycobacterium_avium_subsp_paratuberculosis[, c("V1")]
+  if (nrow(Mycobacterium_avium_subsp_paratuberculosis) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_avium_subsp_paratuberculosis <- data.frame(V1)
+  } else{
+    Mycobacterium_avium_subsp_paratuberculosis <- my_data %>% filter(V5 == 1770)
+    Mycobacterium_avium_subsp_paratuberculosis <-
+      Mycobacterium_avium_subsp_paratuberculosis[, c("V1")]}
+  
+  #Mycobacterium_avium_subsp_silvaticum  44282
+  Mycobacterium_avium_subsp_silvaticum <- my_data %>% filter(V5 == 44282)
+  Mycobacterium_avium_subsp_silvaticum <-
+    Mycobacterium_avium_subsp_silvaticum[, c("V1")]
+  if (nrow(Mycobacterium_avium_subsp_silvaticum) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_avium_subsp_silvaticum <- data.frame(V1)
+  } else{
+    Mycobacterium_avium_subsp_silvaticum <- my_data %>% filter(V5 == 44282)
+    Mycobacterium_avium_subsp_silvaticum <-
+      Mycobacterium_avium_subsp_silvaticum[, c("V1")]}
+  
+  #Mycobacterium_bouchedurhonense 701041
+  Mycobacterium_bouchedurhonense <- my_data %>% filter(V5 == 701041)
+  Mycobacterium_bouchedurhonense <-
+    Mycobacterium_bouchedurhonense[, c("V1")]
+  if (nrow(Mycobacterium_bouchedurhonense) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_bouchedurhonense <- data.frame(V1)
+  } else{
+    Mycobacterium_bouchedurhonense <- my_data %>% filter(V5 == 701041)
+    Mycobacterium_bouchedurhonense <-
+      Mycobacterium_bouchedurhonense[, c("V1")]
+  }
+ 
+  #Mycobacterium_chimaera 701041
+  Mycobacterium_chimaera <- my_data %>% filter(V5 == 701041)
+  Mycobacterium_chimaera <-
+    Mycobacterium_chimaera[, c("V1")]
+  if (nrow(Mycobacterium_chimaera) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_chimaera <- data.frame(V1)
+  } else{
+    Mycobacterium_chimaera <- my_data %>% filter(V5 == 701041)
+    Mycobacterium_chimaera <-
+      Mycobacterium_chimaera[, c("V1")]
+  } 
+  
+  #Mycobacterium_colombiense 1041522
+  Mycobacterium_colombiense <- my_data %>% filter(V5 == 1041522)
+  Mycobacterium_colombiense <-
+    Mycobacterium_colombiense[, c("V1")]
+  if (nrow(Mycobacterium_colombiense) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_colombiense <- data.frame(V1)
+  } else{
+    Mycobacterium_colombiense <- my_data %>% filter(V5 == 1041522)
+    Mycobacterium_colombiense <-
+      Mycobacterium_colombiense[, c("V1")]
+  } 
+  
+  
+  #Mycobacterium_intracellulare 487521
+  Mycobacterium_intracellulare <- my_data %>% filter(V5 == 487521)
+  Mycobacterium_intracellulare <-
+    Mycobacterium_intracellulare[, c("V1")]
+  if (nrow(Mycobacterium_intracellulare) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_intracellulare <- data.frame(V1)
+  } else{
+    Mycobacterium_intracellulare <- my_data %>% filter(V5 == 487521)
+    Mycobacterium_intracellulare <-
+      Mycobacterium_intracellulare[, c("V1")]
+  } 
+  
+  #Mycobacterium_intracellulare_subsp_intracellulare 35617
+  Mycobacterium_intracellulare_subsp_intracellulare <- my_data %>% filter(V5 == 35617)
+  Mycobacterium_intracellulare_subsp_intracellulare <-
+    Mycobacterium_intracellulare_subsp_intracellulare[, c("V1")]
+  if (nrow(Mycobacterium_intracellulare_subsp_intracellulare) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_intracellulare_subsp_intracellulare <- data.frame(V1)
+  } else{
+    Mycobacterium_intracellulare_subsp_intracellulare <- my_data %>% filter(V5 == 35617)
+    Mycobacterium_intracellulare_subsp_intracellulare <-
+      Mycobacterium_intracellulare_subsp_intracellulare[, c("V1")]
+  } 
+  
+  
+  
+  #Mycobacterium_lepraemurium 64667
+  Mycobacterium_lepraemurium <- my_data %>% filter(V5 == 64667)
+  Mycobacterium_lepraemurium <-
+    Mycobacterium_lepraemurium[, c("V1")]
+  if (nrow(Mycobacterium_lepraemurium) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_lepraemurium <- data.frame(V1)
+  } else{
+    Mycobacterium_lepraemurium <- my_data %>% filter(V5 == 64667)
+    Mycobacterium_lepraemurium <-
+      Mycobacterium_lepraemurium[, c("V1")]
+  } 
+  
+  
+  #Mycobacterium_mantenii 560555
+  Mycobacterium_mantenii <- my_data %>% filter(V5 == 560555)
+  Mycobacterium_mantenii <-
+    Mycobacterium_mantenii[, c("V1")]
+  if (nrow(Mycobacterium_mantenii) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_mantenii <- data.frame(V1)
+  } else{
+    Mycobacterium_mantenii <- my_data %>% filter(V5 == 560555)
+    Mycobacterium_mantenii <-
+      Mycobacterium_mantenii[, c("V1")]  }
+  
+    
+  #Mycobacterium_marseillense 701042
+  Mycobacterium_marseillense <- my_data %>% filter(V5 == 701042)
+  Mycobacterium_marseillense <-
+    Mycobacterium_marseillense[, c("V1")]
+  if (nrow(Mycobacterium_marseillense) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_marseillense <- data.frame(V1)
+  } else{
+    Mycobacterium_marseillense <- my_data %>% filter(V5 == 701042)
+    Mycobacterium_marseillense <-
+      Mycobacterium_marseillense[, c("V1")]  }
+  
+  #Mycobacterium_paraintracellulare 1138383
+  Mycobacterium_paraintracellulare <- my_data %>% filter(V5 == 1138383)
+  Mycobacterium_paraintracellulare <-
+    Mycobacterium_paraintracellulare[, c("V1")]
+  if (nrow(Mycobacterium_paraintracellulare) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_paraintracellulare <- data.frame(V1)
+  } else{
+    Mycobacterium_paraintracellulare <- my_data %>% filter(V5 == 1138383)
+    Mycobacterium_paraintracellulare <-
+      Mycobacterium_paraintracellulare[, c("V1")]  }
+  
+  #Mycobacterium_paraintracellulare 1138383
+  Mycobacterium_paraintracellulare <- my_data %>% filter(V5 == 1138383)
+  Mycobacterium_paraintracellulare <-
+    Mycobacterium_paraintracellulare[, c("V1")]
+  if (nrow(Mycobacterium_paraintracellulare) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_paraintracellulare <- data.frame(V1)
+  } else{
+    Mycobacterium_paraintracellulare <- my_data %>% filter(V5 == 1138383)
+    Mycobacterium_paraintracellulare <-
+      Mycobacterium_paraintracellulare[, c("V1")]  }
+  
+  #Mycobacterium_timonense 701043
+  Mycobacterium_timonense <- my_data %>% filter(V5 == 701043)
+  Mycobacterium_timonense <-
+    Mycobacterium_timonense[, c("V1")]
+  if (nrow(Mycobacterium_timonense) == 0) {
+    V1 <- c("not detected")
+    Mycobacterium_timonense <- data.frame(V1)
+  } else{
+    Mycobacterium_timonense <- my_data %>% filter(V5 == 701043)
+    Mycobacterium_timonense <-
+      Mycobacterium_timonense[, c("V1")]  }
   
 # Unestablished species ------------------------------------------------------     
 #  'Mycobacterium_aemonae' 470075
@@ -173,12 +365,20 @@ make_kraken <- function(files) {
 
   kraken_linelist <- data.frame(name,Mycobacterium_album,Mycobacterium_alsense,Mycobacterium_angelicum,
                                 Mycobacterium_aquaticum,Mycobacterium_aquiterrae,Mycobacterium_asiaticum,Mycobacterium_attenuatum,Mycobacterium_avium_complex,
-                                Mycobacterium_arosiense,Mycobacterium_avium,
+                                Mycobacterium_arosiense,Mycobacterium_avium,Mycobacterium_avium_subsp_avium,Mycobacterium_avium_subsp_hominissuis,Mycobacterium_avium_subsp_paratuberculosis,
+                                Mycobacterium_avium_subsp_silvaticum,
+                                Mycobacterium_bouchedurhonense,Mycobacterium_chimaera,Mycobacterium_colombiense,
+                                Mycobacterium_intracellulare,Mycobacterium_intracellulare_subsp_intracellulare,Mycobacterium_lepraemurium,Mycobacterium_mantenii,
+                                Mycobacterium_marseillense,Mycobacterium_paraintracellulare,Mycobacterium_timonense,
                                 Mycobacterium_aemonae,Mycobacterium_anthracenicum)
   
   colnames(kraken_linelist) <- c("sample","Mycobacterium_album","Mycobacterium_alsense","Mycobacterium_angelicum",
                                  "Mycobacterium_aquaticum","Mycobacterium_aquiterrae","Mycobacterium_asiaticum","Mycobacterium_attenuatum","Mycobacterium_avium_complex",
-                                 "Mycobacterium_arosiense","Mycobacterium_avium",
+                                 "Mycobacterium_arosiense","Mycobacterium_avium","Mycobacterium_avium_subsp_avium","Mycobacterium_avium_subsp_hominissuis","Mycobacterium_avium_subsp_paratuberculosis",
+                                 "Mycobacterium_avium_subsp_silvaticum",
+                                 "Mycobacterium_bouchedurhonense","Mycobacterium_chimaera","Mycobacterium_colombiense",
+                                 "Mycobacterium_intracellulare","Mycobacterium_intracellulare_subsp_intracellulare","Mycobacterium_lepraemurium","Mycobacterium_mantenii",
+                                 "Mycobacterium_marseillense","Mycobacterium_paraintracellulare","Mycobacterium_timonense",
                                  "'Mycobacterium_aemonae'","'Mycobacterium_anthracenicum'")
  
 # save the files seperately
@@ -199,4 +399,3 @@ csv_data <- rbindlist(lapply(filenames,fread))
 
 # write the final output to .csv
 write.csv(csv_data,file=paste("./output/kraken_linelist_",outputname,".csv", sep=""), row.names=FALSE)
-
