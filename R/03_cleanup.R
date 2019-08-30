cb7361bd71c20f897b51f919dac64286ad0cc294
dankein <dankein@users.noreply.github.com>
2019-08-30 10:39:34 -0500

    Update 03_cleanup.R

--- R/03_cleanup.R
+++ R/03_cleanup.R
@@ -1,6 +1,13 @@
-#Delete the temp directory
-unlink("./output", recursive = TRUE)
-unlink("./data", recursive = TRUE)
-Sys.sleep(5)
-dir.create("./data")
-dir.create("./output")
+
+#delete tabular files in the data directory
+datadir <- "./data"
+delfiles1 <- dir(path="./data",pattern="*.tabular")
+file.remove(file.path(datadir, delfiles1))
+
+#delete csv files in the output directory
+mydir <- "./output"
+delfiles2 <- dir(path=./output,pattern="*.csv"))
+file.remove(file.path(mydir, delfiles2))
+
+#Delete the output/temp directory to delete it completely
+unlink("./output/temp", recursive = TRUE)
